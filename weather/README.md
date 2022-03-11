c7c838ce36a74686de331a275e7d807759e4cc46
LloydDub <lloydrww@gmail.com>
2022-03-10 21:45:12 -0700

    started building weather app, learning vue structure

---
+++ weather/README.md
@@ -0,0 +1,28 @@
+# weather
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+5e23803e2e209d40d32345f556ff0e96
---
+++ weather/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+   <main>
+     <div class="search-box">
+       <input type="text" class="search" placeholder="Search...">
+       
+     </div>
+   </main>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      api_key: '5e23803e2e209d40d32345f556ff0e96'
+    }
+  }
+
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+   box-sizing: border-box; 
+}
+
+body { 
+  font-family: 'montserrat', sans-serif;
+}
+
+#app {
+  background-image: url('./assets/cold-bg.jpg')
+}
+</style>
